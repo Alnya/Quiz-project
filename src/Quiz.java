ef13283b578b32c28daaac761023468c1c8b0342
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-10 12:33:10 +0900

    Update Quiz.java

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,2 +1,35 @@
 public class Quiz {
+    private final String name;
+    private final String problem;
+    private final String explanation;
+
+    public Quiz(String name, String problem, String explanation) {
+        this.name = name;
+        this.problem = problem;
+        this.explanation = explanation;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getProblem() {
+        return this.problem;
+    }
+
+    public String getExplanation() {
+        return this.explanation;
+    }
+
+    public void printName() {
+
+    }
+
+    public void printProblem() {
+
+    }
+
+    public void printExplanation() {
+
+    }
 }
