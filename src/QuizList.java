5ec25efe39e537606ff474d8225bc96ba6c91b4f
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-10 12:35:41 +0900

    Update QuizList.java

--- src/QuizList.java
+++ src/QuizList.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizList {
+    private List<Quiz> quizList = new ArrayList<Quiz>();
+
+    public QuizList() {
+
+    }
+
+    public Quiz getQuiz(int index) {
+        return this.quizList.get(index);
+    }
 }
