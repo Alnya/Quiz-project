011b8b5ad1342e23e761ca95201f07bc58b19b15
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-10 12:43:33 +0900

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Result {
+    private final List<Quiz> selectedQuizList = new ArrayList<Quiz>();
+    private final List<String> answers = new ArrayList<String>();
+
+    public Result() {
+
+    }
+
+    public void printResult() {
+
+    }
+
+    public int score() {
+        return 0;
+    }
 }
