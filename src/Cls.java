07b0bb0df66ca6a0bed017a0b541b9dbb06b8271
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-11 15:51:12 +0900

    Update Cls.java

--- src/Cls.java
+++ src/Cls.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
 
 public class Cls {
-    public static void clear() throws IOException, InterruptedException {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    public static void clear() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
