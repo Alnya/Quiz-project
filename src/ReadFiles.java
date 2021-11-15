8997f0de3b338f9c7cfea896cf38cab612e68447
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-15 16:41:51 +0900

    Update ReadFiles.java

--- src/ReadFiles.java
+++ src/ReadFiles.java
@@ -1,2 +1,15 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ReadFiles {
+    public static String read(Path path, Charset charset) {
+        try {
+            return new String(Files.readAllBytes(path), charset);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
