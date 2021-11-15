14c687424440ac0b3b44d9e821ee423235d30088
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-15 16:20:32 +0900

    Update Quiz.java

--- src/Quiz.java
+++ src/Quiz.java
@@ -26,28 +26,4 @@ public class Quiz {
     public String getCorrectAnswer() {
         return this.correctAnswer;
     }
-
-    public void printName() {
-        print(this.name);
-    }
-
-    public void printProblem() {
-        print(this.problem);
-    }
-
-    public void printExplanation() {
-        print(this.explanation);
-    }
-
-    public void printCorrectAnswer() {
-        print(this.correctAnswer);
-    }
-
-    private void print(Object obj) {
-        System.out.print(obj);
-    }
-
-    private void println(Object obj) {
-        System.out.println(obj);
-    }
 }
