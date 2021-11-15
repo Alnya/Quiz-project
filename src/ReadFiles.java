d4d1dacb20397e288c6b1ae7e0b0f4577971b0c3
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-15 16:53:23 +0900

    Update ReadFiles.java

--- src/ReadFiles.java
+++ src/ReadFiles.java
@@ -2,8 +2,16 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ReadFiles {
+    public static String readProblem(int index) {
+        String pathName = "../problems/" + String.format("%03d", index);
+        Path path = Paths.get(pathName);
+        Charset charset = Charset.forName("Windows-31J");
+        return readAll(path, charset);
+    }
+
     private static String readAll(Path path, Charset charset) {
         try {
             return new String(Files.readAllBytes(path), charset);
