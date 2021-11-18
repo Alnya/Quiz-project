f150b3d3f7b9cd46de2351b1cf8d79f4ab17176b
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-18 15:03:30 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,32 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        /*
+          Test program of main.
+          read problem, correct-answer and explanation 001.txt
+         */
 
+        String problem001 = ReadFiles.readProblem(1);
+        String correctAnswer001 = ReadFiles.readCorrectAnswer(1);
+        String explanation001 = ReadFiles.readExplanation(1);
+        Quiz quiz001 = new Quiz(String.format("%03d", 1), problem001, explanation001, correctAnswer001);
+        println(quiz001.getProblem());
+        result.addSelectedQuiz(quiz001);
+        String userAnswer = acceptUserAnswer();
+        result.addAnswer(userAnswer);
+        println(result.getScore());
+    }
+
+    private static String acceptUserAnswer() {
+        System.out.print("\n“š‚¦‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢>>");
+        return scanln();
     }
 
     private static String scanln() {
         return scanner.nextLine();
+    }
+
+    private static void println(Object obj) {
+        System.out.println(obj);
     }
 }
