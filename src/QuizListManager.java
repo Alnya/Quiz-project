804b10586ebe232bb7eeadc6e4989a62154700d9
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-22 14:41:27 +0900

    Update QuizListManager.java

--- src/QuizListManager.java
+++ src/QuizListManager.java
@@ -1,7 +1,53 @@
+import java.util.Scanner;
+
 public class QuizListManager {
-    private QuizList quizList;
+    private final QuizList quizList;
+    private final Result result = new Result();
+    private static final Scanner scanner = new Scanner(System.in);
 
     public QuizListManager(QuizList quizList) {
         this.quizList = quizList;
+    }
+
+    public Result runQuiz() {
+        Cls.clear();
+//        println(quiz.getProblem());
+        String userAnswer = acceptUserAnswer();
+        result.addAnswer(userAnswer);
+        printBar();
+//        if (userAnswer.equals(quiz.getCorrectAnswer())) {
+//            println("\t³‰ðIZ");
+//        } else {
+//            println("\t•s³‰ð...~");
+//        }
+        printBar();
+        println("‰ðà‚Ö [Enter]");
+        scanln();
+        printBar();
+//        println(quiz.getExplanation());
+        goNextProblem();
+        return this.result;
+    }
+
+    private static String acceptUserAnswer() {
+        System.out.print("\n“š‚¦‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢>>");
+        return scanln();
+    }
+
+    private static void goNextProblem() {
+        System.out.print("\nŽŸ‚Ì–â‘è‚Ö [Enter]");
+        scanln();
+    }
+
+    private static String scanln() {
+        return scanner.nextLine();
+    }
+
+    private static void printBar() {
+        println("---------------------------------");
+    }
+
+    private static void println(Object obj) {
+        System.out.println(obj);
     }
 }
