b061ec33377fb1ac5917697e3d886178d63d7718
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-22 14:56:14 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,57 +11,16 @@ public class Main {
           run quiz.
          */
         Cls.clear();
-        for (int i = 1; i <= 2; i++) {
+        QuizList quizList = new QuizList();
+        for (int i = 1; i <= 3; i++) {
             String problem = ReadFiles.readProblem(i);
             String correctAnswer = ReadFiles.readCorrectAnswer(i);
             String explanation = ReadFiles.readExplanation(i);
             Quiz quiz = new Quiz(String.format("%03d", i), problem, explanation, correctAnswer);
-            result.addSelectedQuiz(quiz);
+            quizList.addQuiz(quiz);
         }
 
-        for (Quiz quiz : result.getSelectedQuizList()) {
-            runQuiz(quiz);
-        }
-    }
-
-    private static void runQuiz(Quiz quiz) {
-        Cls.clear();
-        println(quiz.getProblem());
-        String userAnswer = acceptUserAnswer();
-        result.addAnswer(userAnswer);
-        printBar();
-        if (userAnswer.equals(quiz.getCorrectAnswer())) {
-            println("\t³‰ðIZ");
-        } else {
-            println("\t•s³‰ð...~");
-        }
-        printBar();
-        println("‰ðà‚Ö [Enter]");
-        scanln();
-        printBar();
-        println(quiz.getExplanation());
-        goNextProblem();
-    }
-
-    private static String acceptUserAnswer() {
-        System.out.print("\n“š‚¦‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢>>");
-        return scanln();
-    }
-
-    private static void goNextProblem() {
-        System.out.print("\nŽŸ‚Ì–â‘è‚Ö [Enter]");
-        scanln();
-    }
-
-    private static String scanln() {
-        return scanner.nextLine();
-    }
-
-    private static void printBar(){
-        println("---------------------------------");
-    }
-
-    private static void println(Object obj) {
-        System.out.println(obj);
+        QuizListManager manager = new QuizListManager(quizList);
+        manager.runQuiz();
     }
 }
