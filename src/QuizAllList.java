c1ec112d2b79f5e18b9fab0f01b9a404d50dce34
Alnya <69311410+Alnya@users.noreply.github.com>
2021-11-29 16:03:54 +0900

    Update QuizAllList.java

--- src/QuizAllList.java
+++ src/QuizAllList.java
@@ -5,7 +5,33 @@ public class QuizAllList {
     private final List<QuizList> quizAllList = new ArrayList<QuizList>();
 
     public QuizAllList() {
-
+        QuizList quizList = new QuizList();
+        for (int i = 1; i <= 5; i++) {
+            String problem = ReadFiles.readProblem(i);
+            String correctAnswer = ReadFiles.readCorrectAnswer(i);
+            String explanation = ReadFiles.readExplanation(i);
+            Quiz quiz = new Quiz(String.format("%03d", i), problem, explanation, correctAnswer);
+            quizList.addQuiz(quiz);
+        }
+        this.quizAllList.add(quizList);
+        QuizList quizList100 = new QuizList();
+        for (int i = 101; i <= 110; i++) {
+            String problem = ReadFiles.readProblem(i);
+            String correctAnswer = ReadFiles.readCorrectAnswer(i);
+            String explanation = ReadFiles.readExplanation(i);
+            Quiz quiz = new Quiz(String.format("%03d", i), problem, explanation, correctAnswer);
+            quizList100.addQuiz(quiz);
+        }
+        this.quizAllList.add(quizList100);
+        QuizList quizList200 = new QuizList();
+        for (int i = 201; i <= 201; i++) {
+            String problem = ReadFiles.readProblem(i);
+            String correctAnswer = ReadFiles.readCorrectAnswer(i);
+            String explanation = ReadFiles.readExplanation(i);
+            Quiz quiz = new Quiz(String.format("%03d", i), problem, explanation, correctAnswer);
+            quizList200.addQuiz(quiz);
+        }
+        this.quizAllList.add(quizList200);
     }
 
     public void addQuizList(QuizList quizList) {
