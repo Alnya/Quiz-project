98df13a3aaef7ed99c6b69149fc1f42258444a5f
Alnya <19aj135@ms.dendai.ac.jp>
2021-12-06 00:18:07 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,6 @@ public class Main {
           read problem, correct-answer and explanation.
           run quiz.
          */
-//        Cls.clear();
-//        QuizList quizList = new QuizList();
-//        for (int i = 1; i <= 3; i++) {
-//            String problem = ReadFiles.readProblem(i);
-//            String correctAnswer = ReadFiles.readCorrectAnswer(i);
-//            String explanation = ReadFiles.readExplanation(i);
-//            Quiz quiz = new Quiz(String.format("%03d", i), problem, explanation, correctAnswer);
-//            quizList.addQuiz(quiz);
-//        }
-//
-//        QuizListManager manager = new QuizListManager(quizList);
-//        result = manager.runQuiz();
         QuizAllList quizAllList = new QuizAllList();
         result = quizAllList.run();
     }
